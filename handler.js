2cb6e7732146e7d82c91b68d2659df100f041e57
zaid <h.zaidamjad29@gmail.com>
2025-09-08 15:42:22 +0500

    deployed using lambda AWS fucntions

---
+++ handler.js
@@ -0,0 +1,27 @@
+const serverless = require("serverless-http");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from root!",
+  });
+});
+
+app.get("/hello", (req, res, next) => {
+  return res.status(200).json({
+    message: "Hello from path!",
+  });
+});
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    error: "Not Found",
+  });
+});
+
+// app.listen(4000, () =>{
+//   console.log('server is running')
+// })
+
+exports.handler = serverless(app);
